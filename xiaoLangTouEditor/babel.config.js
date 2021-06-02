44a04adc6676995ee6d93f379b0c615fc004163b
Administrator <755197142@qq.com>
2021-06-02 09:17:31 +0800

    创建项目

---
+++ xiaoLangTouEditor/babel.config.js
@@ -0,0 +1,15 @@
+module.exports = {
+    presets: [
+        '@vue/cli-plugin-babel/preset'
+    ],
+    plugins: [
+        [
+            "import",
+            {
+                "libraryName": "Vant",
+                "libraryDirectory": "es",
+                "style": true
+            }
+        ]
+    ]
+}
